91fd662d466b611b08143a3d0ee9ea0de7fa995a
Ashin <ashinjimmy54@gmail.com>
2019-12-24 18:39:44 +0530

    Not completed codes

---
+++ HackerRankLearning/src/com/ty/bcits/hackerrankpractice/ComparatorSolution1.java
@@ -0,0 +1,14 @@
+package com.ty.bcits.hackerrankpractice;
+
+import java.util.Scanner;
+
+public class ComparatorSolution1 {
+
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("");
+
+
+	}
+
+}
---
+++ HackerRankLearning/src/com/ty/bcits/hackerrankpractice/HashSetSolution2.java
@@ -0,0 +1,25 @@
+package com.ty.bcits.hackerrankpractice;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HashSetSolution2 {
+
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		
+		System.out.println("Enter the number of pairs");
+		for (int i = 0; i < args.length; i++) {
+			int pairs = Integer.parseInt(sc1.nextLine());
+			System.out.println("Enter the pair of names ");
+			String names = sc1.nextLine();	
+		}
+	
+		
+		
+		
+		
+
+	}
+
+}
